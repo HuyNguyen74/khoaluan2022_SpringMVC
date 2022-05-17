aaaa6a0d80e26cfc51afd000caec19b2306f76fb
HuyNT42 <nguyenthaihuy2802@gmail.com>
2022-05-18 05:16:32 +0700

    update code 18/5

--- src/main/java/khoaluan/controllers/HoaDonCtl.java
+++ src/main/java/khoaluan/controllers/HoaDonCtl.java
@@ -1,12 +1,58 @@
 package khoaluan.controllers;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import khoaluan.entities.ChiTietHoaDon;
+import khoaluan.entities.HoaDon;
+import khoaluan.model.Cart;
+import khoaluan.service.IChiTiet;
+import khoaluan.service.IHoaDon;
+import khoaluan.service.IKhachHang;
+import khoaluan.service.ISanPham;
+
+@RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(path = {"api/"})
 public class HoaDonCtl {
 
+	@Autowired 
+	public IHoaDon Ih;
+	
+	@Autowired
+	public IKhachHang Ik;
+	
+	@Autowired
+	public IChiTiet Ic;
+	
+	@Autowired
+	public ISanPham Is;
+	
+	@PostMapping("order")
+	public boolean orderBill( @RequestParam int customerId, @RequestBody List<Cart> carts) {
+		
+		Timestamp date = new Timestamp(System.currentTimeMillis());
+		try {
+			Ih.save(new HoaDon(0, date, false,Ik.findById(customerId)));
+			List<HoaDon> bills= Ih.findAll();
+			for(Cart c: carts) {
+				Ic.save(new ChiTietHoaDon(0, c.getQuantily(),Is.findById(c.getPhoneId()),bills.get(bills.size()-1) ));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true; 
+	}
 }
--- src/main/java/khoaluan/model/Cart.java
+++ src/main/java/khoaluan/model/Cart.java
@@ -1,5 +1,76 @@
 package khoaluan.model;
 
-public class Cart {
+import javax.persistence.Column;
+import javax.validation.constraints.Min;
 
+import khoaluan.entities.LoaiSanPham;
+
+public class Cart {
+	private long phoneId;
+	private String phoneName;
+	private String ram;
+	private String rom;
+	private String img;
+	private long price;
+	private int quantily;
+	public Cart() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Cart(long phoneId, String phoneName, String ram, String rom, String img, long price, int quantily) {
+		super();
+		this.phoneId = phoneId;
+		this.phoneName = phoneName;
+		this.ram = ram;
+		this.rom = rom;
+		this.img = img;
+		this.price = price;
+		this.quantily = quantily;
+	}
+	public long getPhoneId() {
+		return phoneId;
+	}
+	public void setPhoneId(long phoneId) {
+		this.phoneId = phoneId;
+	}
+	public String getPhoneName() {
+		return phoneName;
+	}
+	public void setPhoneName(String phoneName) {
+		this.phoneName = phoneName;
+	}
+	public String getRam() {
+		return ram;
+	}
+	public void setRam(String ram) {
+		this.ram = ram;
+	}
+	public String getRom() {
+		return rom;
+	}
+	public void setRom(String rom) {
+		this.rom = rom;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public long getPrice() {
+		return price;
+	}
+	public void setPrice(long price) {
+		this.price = price;
+	}
+	public int getQuantily() {
+		return quantily;
+	}
+	public void setQuantily(int quantily) {
+		this.quantily = quantily;
+	}
+	
+	
+
+	
 }
